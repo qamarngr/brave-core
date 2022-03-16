75074ac11b9139e5a4c434db3bbe4dc908da7094
sergey <spylogsster@gmail.com>
2022-03-16 13:50:31 +0300

    Added filecoin network id

--- components/brave_wallet/browser/json_rpc_service_unittest.cc
+++ components/brave_wallet/browser/json_rpc_service_unittest.cc
@@ -1913,6 +1913,7 @@ TEST_F(JsonRpcServiceUnitTest, IsValidDomain) {
 
 TEST_F(JsonRpcServiceUnitTest, GetERC721OwnerOf) {
   bool callback_called = false;
+
   json_rpc_service_->GetERC721OwnerOf(
       "", "0x1", mojom::kMainnetChainId,
       base::BindOnce(&OnStringResponse, &callback_called,
@@ -1943,8 +1944,7 @@ TEST_F(JsonRpcServiceUnitTest, GetERC721OwnerOf) {
   EXPECT_TRUE(callback_called);
 
   SetInterceptor(
-      GetNetwork(mojom::kLocalhostChainId, mojom::CoinType::ETH), "eth_call",
-      "",
+      GetNetwork(mojom::kMainnetChainId, mojom::CoinType::ETH), "eth_call", "",
       "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
       "\"0x000000000000000000000000983110309620d911731ac0932219af0609"
       "1b6744\"}");
@@ -2039,8 +2039,7 @@ TEST_F(JsonRpcServiceUnitTest, GetERC721Balance) {
   EXPECT_TRUE(callback_called);
 
   SetInterceptor(
-      GetNetwork(mojom::kLocalhostChainId, mojom::CoinType::ETH), "eth_call",
-      "",
+      GetNetwork(mojom::kMainnetChainId, mojom::CoinType::ETH), "eth_call", "",
       "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
       "\"0x000000000000000000000000983110309620d911731ac0932219af0609"
       "1b6744\"}");
